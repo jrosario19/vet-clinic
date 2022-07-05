1fda8043eecfbd1d20ddc79ad10bec0716f93a38
Juan Rosario <jrosario19@hotmail.com>
2022-07-05 15:27:46 -0400

    Update animals table

--- schema.sql
+++ schema.sql
@@ -12,3 +12,19 @@ CREATE TABLE animals (
 /* Milestone 2: Add Species Table */
 ALTER TABLE animals
 ADD species varchar(255); 
+
+/* Milestone 3: */
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(255),
+    age integer
+ );
+
+ 
+
+
+ALTER TABLE animals
+DROP COLUMN id;
+
+ALTER TABLE animals
+ADD COLUMN id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY;
