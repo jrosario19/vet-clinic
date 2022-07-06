751e6a0a5b4179e6fe34110e2598a869b7c5f951
Juan Rosario <jrosario19@hotmail.com>
2022-07-06 10:45:21 -0400

    Add aditional table

--- schema.sql
+++ schema.sql
@@ -39,3 +39,29 @@ ADD COLUMN species_id INT REFERENCES species (id);
 
 ALTER TABLE animals
 ADD COLUMN owner_id INT REFERENCES owners (id);
+
+/* Milestone 4 */
+CREATE TABLE vets (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name TEXT,
+    age INT,
+    date_of_graduation DATE,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE specializations (
+    species_id INT,
+    vets_id INT,
+    PRIMARY KEY (species_id, vets_id),
+    FOREIGN KEY (species_id) REFERENCES species (id),
+    FOREIGN KEY (vets_id) REFERENCES vets (id)
+);
+
+CREATE TABLE visits (
+    animals_id INT,
+    vets_id INT,
+    date_of_visit DATE,
+    PRIMARY KEY (animals_id, vets_id, date_of_visit),
+    FOREIGN KEY (animals_id) REFERENCES animals (id),
+    FOREIGN KEY (vets_id) REFERENCES vets (id)
+);
